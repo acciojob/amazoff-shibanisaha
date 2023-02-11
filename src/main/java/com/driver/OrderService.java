3a39abeeb2bfd4796ed0d9203400e4e9253c1b19
shibanisaha <shibanis138@gmail.com>
2023-02-11 17:40:31 +0530

    commit without autowire

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -10,8 +10,8 @@ import java.util.List;
 @Service
 public class OrderService {
 
-    @Autowired
-    OrderRepository orderRepository;
+
+    OrderRepository orderRepository = new OrderRepository();
 
     public void addOrder(Order order){
         orderRepository.addOrder(order);
